0b1d4b652293bb927853523798dd632c8190b670
Hans <admin@itsrv.tw>
2023-08-25 14:52:34 +0800

    chore: make example videos start play at same time

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,15 +3,31 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { VideoPlayer } from '@fugood/react-native-video-player';
 
+const NUM = 5;
+
 export default function App() {
+  const [num, setNum] = React.useState(0);
+
+  const ready = React.useCallback(() => {
+    setNum((n) => n + 1);
+    console.log(`[ready] ${performance.now()}`);
+  }, []);
+
+  React.useEffect(() => {
+    console.log(`[render] ${performance.now()}`);
+  }, []);
+
   return (
     <View style={styles.container}>
-      {new Array(5).fill(0).map((_, i) => (
+      {new Array(NUM).fill().map((_, i) => (
         <VideoPlayer
           key={i}
           url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
           style={styles.box}
-          loop={true}
+          onReady={ready}
+          onError={console.error}
+          loop
+          paused={num < NUM}
         />
       ))}
     </View>
